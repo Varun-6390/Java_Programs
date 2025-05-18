fcb47536d30a0d267c08df4663bc39d098f79a17
Varun-6390 <artistvarunlko@gmail.com>
2025-05-18 22:13:02 +0530

    Arithmetic Operators in java

---
+++ Program_5.java
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+
+public class Program_5 {
+    public static void main(String[] args) {
+
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y; // ADDITION
+        z = x - y; // SUBTRACTION
+        z = x * y; // MULTIPLICATION
+        z = x / y; // DIVISION
+        z = x % y; // MODULUS
+
+        System.out.println(z);
+
+    }
+}

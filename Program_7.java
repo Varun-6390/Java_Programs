af05bc6f269bbdc715f427082d6c20263a6e8536
Varun-6390 <artistvarunlko@gmail.com>
2025-05-19 22:27:10 +0530

    if else in java

---
+++ Program_7.java
@@ -0,0 +1,14 @@
+public class Program_7 {
+    // if statement = performs a block of code if its condition is true
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        
+        if(isStudent){
+            System.out.println("You are a student!");
+        }
+        else{
+            System.out.println("You are NOT a student");
+        }
+    }
+        
+}

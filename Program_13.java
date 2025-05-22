2a0989214928b91cab86f999ac30fbdff5470ead
Varun-6390 <artistvarunlko@gmail.com>
2025-05-22 21:42:47 +0530

    Strings in java

---
+++ Program_13.java
@@ -0,0 +1,40 @@
+// Strings in java
+
+public class Program_13 {
+    public static void main(String[] args) {
+
+        String name = "Harry Putter";
+
+        int length = name.length();
+        char letter = name.charAt(0);
+        int index = name.indexOf(" "); 
+        int lastIndex = name.lastIndexOf("r");
+
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("u", "o");
+
+        if (name.isEmpty()) {
+            System.out.println("Your name is empty");
+        } else {
+            System.out.println("Hello " + name);
+        }
+
+        if (name.contains(" ")) {
+            System.out.println("Your name contains a space");
+        } else {
+            System.out.println("Your name DOESN'T contain any spaces");
+        }
+
+        if (name.equalsIgnoreCase("password")) {
+            System.out.println("Your name can't be password");
+        } else {
+            System.out.println("Hello " + name);
+            System.out.println(length);
+            System.out.println(letter);
+            System.out.println(index);
+            System.out.println(lastIndex);
+        }
+    }
+}

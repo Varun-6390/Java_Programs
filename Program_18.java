ee8e4f78dc1038a38dd2cee93074b7623ecf8fc2
Varun-6390 <artistvarunlko@gmail.com>
2025-05-24 21:28:38 +0530

    Java enhanced switch case

---
+++ Program_18.java
@@ -0,0 +1,23 @@
+// Java enhanced switch case
+
+import java.util.Scanner;
+
+public class Program_18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the day: ");
+        String day = scanner.nextLine();
+
+        scanner.close();
+
+        switch (day.toLowerCase()) {
+            case "monday", "tuesday", "wednesday", "thursday", "friday" ->
+                System.out.println("Not a weekend");
+
+            case "saturday", "sunday" ->
+                System.out.println("Weekend");
+
+            default -> System.out.println("Not a day");
+        }
+    }
+}

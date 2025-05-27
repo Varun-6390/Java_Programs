f7e5fd45ee2580cc64b1036d41a18c2b6d43e033
Varun-6390 <artistvarunlko@gmail.com>
2025-05-27 21:39:48 +0530

    for loop in java

---
+++ Program_23.java
@@ -0,0 +1,24 @@
+// for loop in java
+
+import java.util.Scanner;
+
+public class Program_23 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // for loop = execute some code a CERTAIN amount of times
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Time's up!");
+        scanner.close();
+    }
+    
+}

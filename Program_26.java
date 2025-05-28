1f8e13e68d632afaa9442d39c18f0da05e1b7e55
Varun-6390 <artistvarunlko@gmail.com>
2025-05-28 22:10:19 +0530

    method in java

---
+++ Program_26.java
@@ -0,0 +1,31 @@
+public class Program_26 {
+     public static void main(String[] args){
+
+        // method =  a block of reusable code that is executed when called ()
+
+        happyBirthday("Albus", 80);
+    }
+    static void happyBirthday(String name, int age){
+        System.out.println("Happy Birthday to you!");
+        System.out.printf("Happy Birthday dear %s!\n", name);
+        System.out.printf("You are %d years old!\n", age);
+        System.out.println("Happy Birthday to you!\n");
+    }
+    static double square(double number){
+        return number * number;
+    }
+    static double cube(double number){
+        return number * number * number;
+    }
+    static String getFullName(String first, String last){
+        return first + " " + last;
+    }
+    static boolean ageCheck(int age){
+        if(age >= 18){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

7a42143311fe18858397374ce77cee90f37f33ef
Varun-6390 <artistvarunlko@gmail.com>
2025-06-08 21:46:54 +0530

    Inheritance (Part-c)

---
+++ Program_44_c.java
@@ -0,0 +1,9 @@
+// Plant class (parent)
+
+public class Program_44_c extends Program_44_b{
+    
+    void photosynthesis()
+    {
+        System.out.println("The plants absorb sunlight");
+    }
+}

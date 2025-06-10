7aeea35962b44026a71869c65117aa39ce318acb
Varun-6390 <artistvarunlko@gmail.com>
2025-06-10 22:18:07 +0530

    super in java (Part-2)

---
+++ Program_45_b.java
@@ -0,0 +1,14 @@
+public class Program_45_b {
+
+    String first;
+    String last;
+
+    Program_45_b(String first, String last){
+        this.first = first;
+        this.last = last;
+    }
+
+    void showName(){
+        System.out.println(this.first + " " + this.last);
+    }
+}

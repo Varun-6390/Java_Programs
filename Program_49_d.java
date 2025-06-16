8353b28ec15d19a146111acbcb459402b234ad60
Varun-6390 <artistvarunlko@gmail.com>
2025-06-16 22:41:22 +0530

    Interface (Part-4)

---
+++ Program_49_d.java
@@ -0,0 +1,9 @@
+// rabbit class (child)
+
+public class Program_49_d implements Program_49_b {
+
+    @Override
+    public void flee() {
+        System.out.println("The rabbit is running");
+    }
+}

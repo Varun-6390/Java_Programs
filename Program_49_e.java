9c6a2d5d852ec041da97ff9eccb41169efcb8ded
Varun-6390 <artistvarunlko@gmail.com>
2025-06-17 21:36:26 +0530

    Interface (Part-5)

---
+++ Program_49_e.java
@@ -0,0 +1,10 @@
+// Hawk class (child)
+
+public class Program_49_e implements Program_49_c {
+
+    @Override
+    public void hunt() {
+        System.out.println("The Hawk is hunting");
+    }
+
+}

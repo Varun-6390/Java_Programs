2a92b1b14d2d20b6d3f7604dae42f2aefa85aa5a
Varun-6390 <artistvarunlko@gmail.com>
2025-06-19 21:21:05 +0530

    Polymorphism (Part-4)

---
+++ Program_50_d.java
@@ -0,0 +1,9 @@
+// Car Class (Child)Add commentMore actions
+
+public class Program_50_d extends Program_50_b{
+    
+    @Override
+    void go(){
+        System.out.println("You are driving the car");
+    }
+}

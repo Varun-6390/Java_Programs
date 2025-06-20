f84ba0a407bff310e41e029abc65155839037569
Varun-6390 <artistvarunlko@gmail.com>
2025-06-20 22:30:02 +0530

    Polymorphism (Part-5)

---
+++ Program_50_e.java
@@ -0,0 +1,9 @@
+// Boat class (child)Add commentMore actions
+
+public class Program_50_e extends Program_50_b {
+
+    @Override
+    void go() {
+        System.out.println("You are sailing the boat");
+    }
+}

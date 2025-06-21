594d32c7df4378166b6dc3a85dfb2c0b2916350d
Varun-6390 <artistvarunlko@gmail.com>
2025-06-21 21:08:15 +0530

    Runtime polymorphism (Part-3)

---
+++ Program_51_c.java
@@ -0,0 +1,11 @@
+// Dog class (child)Add commentMore actions
+
+public class Program_51_c extends Program_51_b{
+
+    @Override
+    void speak()
+    {
+        System.out.println("The dog is barking");
+    }
+    
+}

17e9aaf435f2e85b0611684d3709581a9c893161
Varun-6390 <artistvarunlko@gmail.com>
2025-06-22 21:47:32 +0530

    Runtime polymorphism (Part-4)

---
+++ Program_51_d.java
@@ -0,0 +1,8 @@
+// Cat class (child)Add commentMore actions
+
+public class Program_51_d extends Program_51_b {
+    @Override
+    void speak() {
+        System.out.println("The cat meows");
+    }
+}

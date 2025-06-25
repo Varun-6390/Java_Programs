b0e9963b122245b79017f8033c7a098ff250dd74
Varun-6390 <artistvarunlko@gmail.com>
2025-06-25 21:21:21 +0530

    Composition (Part-3)

---
+++ Program_54_c.java
@@ -0,0 +1,12 @@
+public class Program_54_c {
+    
+    String type;
+
+    Program_54_c(String type){
+        this.type = type;
+    }
+
+    void start(){
+        System.out.println("You start the " + this.type + " engine");
+    }
+}

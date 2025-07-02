123b376b336f0800269c0c93e732d2570b6e2a9f
Varun-6390 <artistvarunlko@gmail.com>
2025-07-02 22:19:43 +0530

    Generics (Part-2)

---
+++ Program_66_b.java
@@ -0,0 +1,17 @@
+public class Program_66_b<T, U> {
+    T item;
+    U price;
+
+    Program_66_b(T item, U price) {
+        this.item = item;
+        this.price = price;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+
+    public U getPrice() {
+        return this.price;
+    }
+}

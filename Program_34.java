38556e2da649b5898c5ae865c24f81a101da71fd
Varun-6390 <artistvarunlko@gmail.com>
2025-07-09 22:09:16 +0530

    Varargs in java

--- Program_34.java
+++ Program_34.java
@@ -1,13 +1,11 @@
-public class Program_34 {
-    public static void main(String[] args) {
-
         // varargs = allow a method to accept a varying # of arguments
         // makes methods more flexible, no need for overloaded methods
         // java will pack the arguments into an array
         // ... (ellipsis)
 
+public class Program_34 {
+    public static void main(String[] args) {
         System.out.println(average(1, 2, 3, 4));
-
     }
 
     static double average(double... numbers) {

4ee49d47085785c4b828bfffe0831af5a2baea22
Varun-6390 <artistvarunlko@gmail.com>
2025-07-11 21:49:09 +0530

    Method overriding

--- Program_46.java
+++ Program_46.java
@@ -1,9 +1,11 @@
+        // Method overriding = When a subclass provides its own
+        //implementation of a method that is already defined.
+        //Allows for code reusability and gives specific implementations.
+
+
 public class Program_46 {
      public static void main(String[] args) {
 
-        // Method overriding = When a subclass provides its own
-        //                                      implementation of a method that is already defined.
-        //                                      Allows for code reusability and gives specific implementations.
 
         Dog dog = new Dog();
         Cat cat = new Cat();

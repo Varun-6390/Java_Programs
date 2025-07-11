7e8115555a134f98fd854dedeb18ad28218d1fe5
Varun-6390 <artistvarunlko@gmail.com>
2025-07-11 21:47:14 +0530

    Arrays in java

--- Program_31.java
+++ Program_31.java
@@ -1,8 +1,9 @@
+        // array = a collection of values of the same data type
+        // * think of it as a variable that can store more than 1 value *
+
 public class Program_31 {
     public static void main(String[] args) {
 
-        // array = a collection of values of the same data type
-        // * think of it as a variable that can store more than 1 value *
 
         String[] fruits = { "apple", "orange", "banana", "coconut" };
 

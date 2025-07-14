1b97c0a87a1c0f85f22e6d32b3dde18143baa067
Varun-6390 <artistvarunlko@gmail.com>
2025-07-14 21:49:23 +0530

    ternary operator

--- Program_16.java
+++ Program_16.java
@@ -1,9 +1,9 @@
-public class Program_16 {
-    public static void main(String[] args) {
 
         // ternary operator ? = Return 1 of 2 values if a condition is true
 
         // variable = (condition) ? IfTrue : IfFalse;
+public class Program_16 {
+    public static void main(String[] args) {
 
         int number = 10;
 

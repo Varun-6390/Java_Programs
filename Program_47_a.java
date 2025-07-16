4a685fd000a4d2b213d10e3ad3a5813d0da16f4a
Varun-6390 <artistvarunlko@gmail.com>
2025-07-16 22:20:32 +0530

    toString() method

--- Program_47_a.java
+++ Program_47_a.java
@@ -1,10 +1,11 @@
-public class Program_47_a {
-    public static void main(String[] args) {
-
         // .toString() = Method inherited from the Object class.
         // Used to return a string representation of an object.
         // By default, it returns a hash code as a unique identifier.
         // It can be overridden to provide meaningful details.
+
+public class Program_47_a {
+    public static void main(String[] args) {
+
 
         Program_47_b car1 = new Program_47_b("Ford", "Mustang", 2025, "Red");
         Program_47_b car2 = new Program_47_b("Chevrolet", "Corvette", 2026, "Blue");

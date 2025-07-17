1fd47d55c41ca857eac8905c4d826895586c794a
Varun-6390 <artistvarunlko@gmail.com>
2025-07-17 21:47:10 +0530

    Exception handling in java

--- Program_57.java
+++ Program_57.java
@@ -1,13 +1,13 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class Program_57 {
-     public static void main(String[] args) {
 
         // Exception = An event that interrupts the normal flow of a program
         //                      (Dividing by zero, file not found, mismatch input type)
         //                      Surround any dangerous code with a try{} block
         //                      try{}, catch{}, finally{}
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program_57 {
+     public static void main(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter a number: ");

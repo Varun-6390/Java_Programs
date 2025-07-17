03da926e4fda9db7fd3f48a9ed1d27e0acdfc8f5
Varun-6390 <artistvarunlko@gmail.com>
2025-07-17 21:48:07 +0530

    Exception handling in java

--- Program_57.java
+++ Program_57.java
@@ -1,8 +1,8 @@
+// Exception = An event that interrupts the normal flow of a program
+//                      (Dividing by zero, file not found, mismatch input type)
+//                      Surround any dangerous code with a try{} block
+//                      try{}, catch{}, finally{}
 
-        // Exception = An event that interrupts the normal flow of a program
-        //                      (Dividing by zero, file not found, mismatch input type)
-        //                      Surround any dangerous code with a try{} block
-        //                      try{}, catch{}, finally{}
 import java.util.InputMismatchException;
 import java.util.Scanner;
 

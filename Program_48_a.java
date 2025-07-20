db8ac0623d6e2c7b42312e38e417739a09066249
Varun-6390 <artistvarunlko@gmail.com>
2025-07-20 21:53:00 +0530

    Abstraction (Part-1)

--- Program_48_a.java
+++ Program_48_a.java
@@ -1,12 +1,12 @@
-public class Program_48_a {
-    public static void main(String[] args) {
-
         // abstract = Used to define abstract classes and methods.
         // Abstraction is the process of hiding implementation details
         // and showing only the essential features.
         // Abstract classes CAN'T be instantiated directly.
         // Can contain 'abstract' methods (which must be implemented)
         // Can contain 'concrete' methods (which are inherited)
+public class Program_48_a {
+    public static void main(String[] args) {
+
 
         Program_48_c circle = new Program_48_c(3);
         Program_48_d triangle = new Program_48_d(4, 5);

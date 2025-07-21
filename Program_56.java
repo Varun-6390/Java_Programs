914ec9a4691143803ec506f0ee6d38a1278fe275
Varun-6390 <artistvarunlko@gmail.com>
2025-07-21 22:39:55 +0530

    ArrayList in java

--- Program_56.java
+++ Program_56.java
@@ -1,11 +1,11 @@
+        // ArrayList = A resizeable array that stores objects (autoboxing)
+        //                     Arrays are fixed in size, but ArrayLists can change
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Program_56 {
       public static void main(String[] args) {
 
-        // ArrayList = A resizeable array that stores objects (autoboxing)
-        //                     Arrays are fixed in size, but ArrayLists can change
 
         ArrayList<String> fruits = new ArrayList<>();
 

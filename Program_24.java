a47080fba27f26995ba6168287089d927f36fceb
Varun-6390 <artistvarunlko@gmail.com>
2025-07-26 21:36:49 +0530

    break and continue

--- Program_24.java
+++ Program_24.java
@@ -1,10 +1,10 @@
 // break and continue
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
 
 public class Program_24 {
     public static void main(String[] args){
         
-        // break = break out of a loop (STOP)
-        // continue = skip current iteration of a loop (SKIP)
 
         for(int i = 0; i < 10; i++){
             if(i == 4){
